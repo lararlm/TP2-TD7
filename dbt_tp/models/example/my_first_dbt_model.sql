fb66e2e53a6eba93bb87aad74e9acc53aace0011
lararlm <lararodrigueslamoglie@gmail.com>
2025-06-23 12:53:52 -0300

    dbt

--- dbt_tp/models/example/my_first_dbt_model.sql
+++ dbt_tp/models/example/my_first_dbt_model.sql
@@ -1,11 +1,9 @@
--- file: dbt_tp/models/marts/mart_proporcion_genero_denunciantes.sql
 {{
     config(
         materialized='table'
     )
 }}
 
--- Paso 1: Contar el número de denunciantes por género
 WITH gender_counts AS (
     SELECT
         genero,
@@ -16,7 +14,6 @@ WITH gender_counts AS (
         genero
 ),
 
--- Paso 2: Contar el total de denunciantes
 total_counts AS (
     SELECT
         COUNT(dni) AS total_denunciantes
@@ -24,16 +21,13 @@ total_counts AS (
         {{ source('raw_data', 'Solicitante') }}
 )
 
--- Paso 3: Calcular la proporción y presentar el resultado final
 SELECT
     gc.genero,
     gc.numero_de_denunciantes,
     tc.total_denunciantes,
-    -- Calculamos el porcentaje y lo redondeamos a 2 decimales
     ROUND((gc.numero_de_denunciantes::numeric / tc.total_denunciantes) * 100, 2) AS proporcion_porcentual
 FROM
     gender_counts gc
--- Usamos CROSS JOIN para aplicar el total a cada fila de género
 CROSS JOIN
     total_counts tc
 ORDER BY
